32c8dbf5dcf0f535cfaca2e5cca78ebcef3f2364
Wanli <zhangwanly@gmail.com>
2026-06-14 11:28:17 +0800

    init

---
+++ lib/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    api(libs.commons.math3)
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
